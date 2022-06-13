ede370fce4c16b8de19e8fdcd939b062bc50b42f
LoneDev6 <leoxy98@gmail.com>
2022-06-13 15:32:01 +0200

    1.19 support

---
+++ nms_v1_19_R1/src/main/java/dev/lone/blocksinjector/custom_blocks/nms/blocksmanager/v1_19_R1.java
@@ -0,0 +1,265 @@
+package dev.lone.blocksinjector.custom_blocks.nms.blocksmanager;
+
+import com.comphenix.protocol.ProtocolLibrary;
+import dev.lone.LoneLibs.nbt.nbtapi.NBTItem;
+import dev.lone.blocksinjector.IrisHook;
+import dev.lone.blocksinjector.Settings;
+import dev.lone.blocksinjector.custom_blocks.CachedCustomBlockInfo;
+import dev.lone.blocksinjector.custom_blocks.nms.Nms;
+import dev.lone.itemsadder.api.CustomBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.core.MappedRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_19_R1.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_19_R1.block.data.CraftBlockData;
+import org.bukkit.craftbukkit.v1_19_R1.util.CraftMagicNumbers;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class v1_19_R1 extends AbstractCustomBlocksManager<Block, BlockState, ClientboundLevelChunkPacketData>
+{
+    public v1_19_R1()
+    {
+        try
+        {
+            field_BLOCK_MATERIAL = CraftMagicNumbers.class.getDeclaredField("BLOCK_MATERIAL");
+            field_BLOCK_MATERIAL.setAccessible(true);
+        }
+        catch (NoSuchFieldException e)
+        {
+            e.printStackTrace();
+        }
+        BUFFER_FIELD = ClientboundLevelChunkPacketData.class.getDeclaredFields()[2];
+        BUFFER_FIELD.setAccessible(true);
+    }
+
+    @Override
+    public void load(Plugin plugin, HashMap<CachedCustomBlockInfo, Integer> namespacedBlocks)
+    {
+        this.plugin = plugin;
+
+        injectBlocks(namespacedBlocks);
+    }
+
+    /**
+     * Load namespacedIds from IA yml cached ids files
+     */
+    @Override
+    public void loadFromCache()
+    {
+        String pluginsFolder = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        File storageFolder = new File(new File(pluginsFolder, "ItemsAdder"), "storage");
+        if(storageFolder.exists())
+        {
+
+            HashMap<CachedCustomBlockInfo, Integer> namespacedBlocks = new HashMap<>();
+            namespacedBlocks.putAll(loadCacheFile(storageFolder, "real_blocks_ids_cache"));
+            namespacedBlocks.putAll(loadCacheFile(storageFolder, "real_blocks_note_ids_cache"));
+            namespacedBlocks.putAll(loadCacheFile(storageFolder, "real_transparent_blocks_ids_cache"));
+            namespacedBlocks.putAll(loadCacheFile(storageFolder, "real_wire_ids_cache"));
+
+            load(plugin, namespacedBlocks);
+        }
+        else
+        {
+            throw new RuntimeException("ItemsAdder not installed");
+        }
+    }
+
+    @Override
+    public void registerListener(Plugin plugin)
+    {
+        this.plugin = plugin;
+        if(packet == null)
+        {
+            packet = new dev.lone.blocksinjector.custom_blocks.nms.packetlistener.v1_19_R1(plugin);
+            ProtocolLibrary.getProtocolManager().addPacketListener(packet);
+        }
+    }
+
+    @Override
+    void injectBlocks(HashMap<CachedCustomBlockInfo, Integer> customBlocks)
+    {
+        if(Settings.debug)
+            Bukkit.getLogger().info("Injecting " + customBlocks.size() + " blocks...");
+
+        unfreezeRegistry();
+        customBlocks.forEach((cached, integer) -> {
+
+            Block internalBlock = isBlockAlreadyRegistered(cached);
+            if(internalBlock != null)
+            {
+                if(Settings.debug)
+                    Bukkit.getLogger().warning("Block '" + internalBlock.getDescriptionId() + "' already registered, skipping.");
+            }
+            else
+            {
+                try
+                {
+                    internalBlock = new Block(BlockBehaviour.Properties.copy(Blocks.COBBLESTONE));
+
+                    //<editor-fold desc="Inject the block into the Minecraft internal registry">
+                    Registry.register(
+                            Registry.BLOCK,
+                            new ResourceLocation(cached.namespace, cached.key),
+                            internalBlock
+                    );
+                    internalBlock.getStateDefinition().getPossibleStates().forEach(Block.BLOCK_STATE_REGISTRY::add);
+                    if(Settings.debug)
+                        Bukkit.getLogger().info("Injected block into Minecraft Registry.BLOCK: " + internalBlock.getDescriptionId());
+                    //</editor-fold>
+
+                    //<editor-fold desc="Inject the block into the Bukkit lookup data structures to avoid incompatibilities with plugins">
+                    try
+                    {
+                        HashMap<Block, Material> BLOCK_MATERIAL = (HashMap<Block, Material>) field_BLOCK_MATERIAL.get(null);
+                        BLOCK_MATERIAL.put(internalBlock, Material.COBBLESTONE);
+
+                        if(Settings.debug)
+                            Bukkit.getLogger().info("Injected block into Bukkit lookup: " + internalBlock.getDescriptionId());
+                    }
+                    catch (IllegalAccessException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    //</editor-fold>
+                }
+                catch (IllegalStateException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            if(internalBlock != null)
+            {
+                registeredBlocks.put(internalBlock, cached);
+                registeredBlocks_stateIds.put(Block.BLOCK_STATE_REGISTRY.getId(internalBlock.defaultBlockState()), internalBlock);
+            }
+
+        });
+
+        Blocks.rebuildCache();
+        Registry.BLOCK.freeze();
+
+        if(Bukkit.getPluginManager().getPlugin("Iris") != null)
+            IrisHook.inject(customBlocks);
+
+        if(Settings.debug)
+            Bukkit.getLogger().info("Finished injecting blocks");
+    }
+
+    @Override
+    void unfreezeRegistry()
+    {
+        try
+        {
+            Field intrusiveHolderCache = Nms.getField(MappedRegistry.class, Map.class, 5);
+            intrusiveHolderCache.setAccessible(true);
+            intrusiveHolderCache.set(Registry.BLOCK, new IdentityHashMap<Block, Holder.Reference<Block>>());
+
+            Field frozen = Nms.getField(MappedRegistry.class, boolean.class, 0);
+            frozen.setAccessible(true);
+            frozen.set(Registry.BLOCK, false);
+        }
+        catch (SecurityException | IllegalArgumentException | IllegalAccessException | NullPointerException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void fixBlockInteract(PlayerInteractEvent e)
+    {
+        Location blockLoc = e.getClickedBlock().getLocation();
+        String descriptionId = ((CraftBlock) e.getClickedBlock()).getHandle().getBlockState(new BlockPos(blockLoc.getBlockX(), blockLoc.getBlockY(), blockLoc.getBlockZ())).getBlock().getDescriptionId();
+        String namespacedId = descriptionId.replace("block.", "").replace(".", ":");
+        CustomBlock customBlock = CustomBlock.getInstance(namespacedId);
+        if(customBlock != null) // Is a custom injected block
+        {
+            Player player = e.getPlayer();
+            if(player.getGameMode() == GameMode.CREATIVE)
+            {
+                customBlock.place(e.getClickedBlock().getLocation());
+            }
+            else
+            {
+                //<editor-fold desc="This is a fucking cheat to make the dig animation stop and avoid getting stuck breaking the block.">
+                e.setCancelled(true);
+                AtomicReference<ItemStack> prev = new AtomicReference<>();
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    prev.set(player.getItemInHand());
+
+                    ItemStack tmp = prev.get().clone();
+                    NBTItem n = new NBTItem(tmp);
+                    n.setBoolean("sus", true);
+
+                    player.setItemInHand(n.getItem());
+                }, 1L);
+
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    player.setItemInHand(prev.get());
+                    customBlock.place(e.getClickedBlock().getLocation());
+                }, 2L);
+                //</editor-fold>
+            }
+        }
+    }
+
+    @Override
+    public BlockState nmsBlockFromCached(CachedCustomBlockInfo cachedBlock)
+    {
+        //TODO: handle spawner blocks, I don't care, I won't support them. They are TILE entities and harder to support.
+        CraftBlockData bukkitData = (CraftBlockData) getItemsAdderBlockDataByInternalId(cachedBlock.id);
+        return bukkitData.getState();
+    }
+
+    @Override
+    public BlockState nmsBlockStateFromBlockNamespacedId(int id)
+    {
+        //TODO: handle spawner blocks, I don't care, I won't support them. They are TILE entities and harder to support.
+        CraftBlockData bukkitData = (CraftBlockData) getItemsAdderBlockDataByInternalId(id);
+        return bukkitData.getState();
+    }
+
+    @Override
+    @Nullable
+    Block isBlockAlreadyRegistered(CachedCustomBlockInfo cached) // Is this function even making sense?
+    {
+        try
+        {
+            Bukkit.createBlockData(cached.getNamespacedId());
+        }
+        catch(IllegalArgumentException e) // Block not registered
+        {
+            // TODO: recode this shit
+            for (BlockState state : Block.BLOCK_STATE_REGISTRY)
+            {
+                Block internalBlock = state.getBlock();
+                if (internalBlock.getDescriptionId().equals("block." + cached.namespace + "." + cached.key))
+                    return internalBlock;
+            }
+        }
+        return null;
+    }
+}
---
+++ nms_v1_19_R1/src/main/java/dev/lone/blocksinjector/custom_blocks/nms/packetlistener/v1_19_R1.java
@@ -0,0 +1,213 @@
+package dev.lone.blocksinjector.custom_blocks.nms.packetlistener;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.viaversion.viaversion.api.minecraft.chunks.ChunkSection;
+import com.viaversion.viaversion.api.minecraft.chunks.DataPalette;
+import com.viaversion.viaversion.api.minecraft.chunks.PaletteType;
+import com.viaversion.viaversion.api.type.types.version.ChunkSectionType1_18;
+import dev.lone.blocksinjector.custom_blocks.CachedCustomBlockInfo;
+import dev.lone.blocksinjector.custom_blocks.nms.blocksmanager.AbstractCustomBlocksManager;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.core.Registry;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkPacketData;
+import net.minecraft.network.protocol.game.ClientboundLevelChunkWithLightPacket;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.bukkit.World;
+import org.bukkit.plugin.Plugin;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class v1_19_R1 extends AbstractPacketListener
+{
+    ChunkSectionType1_18 chunkSectionType = new ChunkSectionType1_18(Block.BLOCK_STATE_REGISTRY.size(), Registry.BIOME_SOURCE.size());
+
+    public v1_19_R1(Plugin plugin)
+    {
+        super(new PacketAdapter.AdapterParameteters()
+                      .plugin(plugin)
+                      .listenerPriority(ListenerPriority.LOWEST)
+                      .optionAsync()
+                      .types(
+                              PacketType.Play.Server.MAP_CHUNK,
+                              PacketType.Play.Server.BLOCK_CHANGE,
+                              PacketType.Play.Server.MULTI_BLOCK_CHANGE
+                      ));
+    }
+
+    // https://gist.github.com/MrPowerGamerBR/51bf5beb6466d557da2191ed8a3fe0df
+    @Override
+    public void onPacketSending(PacketEvent e)
+    {
+        PacketContainer packet = e.getPacket();
+        // Should I catch not registered blocks (removed from configs) and render them as STONE (for example), to avoid clients crash?
+        // Seems the game automatically removes unknown blocks from the region files.
+
+        if (e.getPacketType() == PacketType.Play.Server.MAP_CHUNK)
+        {
+            ClientboundLevelChunkWithLightPacket handle = (ClientboundLevelChunkWithLightPacket) packet.getHandle();
+            ClientboundLevelChunkPacketData chunkDataPacket = handle.getChunkData();
+
+            World world = e.getPlayer().getWorld();
+            int worldMinHeight = world.getMinHeight();
+            int worldMaxHeight = world.getMaxHeight();
+            int worldTrueHeight = (Math.abs(worldMinHeight) + worldMaxHeight);
+            int ySectionCount = worldTrueHeight / 16;
+
+            // And then we read the byte array from there!
+            FriendlyByteBuf readBuffer = chunkDataPacket.getReadBuffer();
+            byte[] byteArray = readBuffer.array();
+            ByteBuf buf = Unpooled.copiedBuffer(byteArray);
+
+            List<ChunkSection> sections = new ArrayList<>();
+            for (int i = 0; i < ySectionCount; i++)
+            {
+                try
+                {
+                    sections.add(chunkSectionType.read(buf));
+                }
+                catch (Exception ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+
+            AtomicBoolean requiresEdits = new AtomicBoolean();
+            sections.forEach(section -> {
+                @Nullable DataPalette blockPalette = section.palette(PaletteType.BLOCKS);
+                if (blockPalette == null) // Does not have any palette...
+                    return;
+
+                if(containsNoCustomBlock(blockPalette))
+                    return;
+
+                boolean hasCustomBlocks = false;
+
+                //TODO: Use this instead, probably more optimized.
+                // Basically only change the palette entry instead of iterating the whole-ass 16x16x16 chunk section.
+//                for (int i = 0; i < section.getPaletteSize(); i++)
+//                {
+//                    section.replacePaletteEntry(i, );
+//                }
+
+                for (int y = 0; y < 16; y++)
+                {
+                    for (int x = 0; x < 16; x++)
+                    {
+                        for (int z = 0; z < 16; z++)
+                        {
+                            BlockState blockData = Block.BLOCK_STATE_REGISTRY.byId(section.getFlatBlock(x, y, z));
+                            if (blockData != null)
+                            {
+                                CachedCustomBlockInfo cachedBlock = AbstractCustomBlocksManager.inst.get(blockData.getBlock());
+                                if (cachedBlock != null)
+                                {
+                                    section.setFlatBlock(
+                                            x,
+                                            y,
+                                            z,
+                                            Block.BLOCK_STATE_REGISTRY.getId((BlockState) AbstractCustomBlocksManager.inst.nmsBlockFromCached(cachedBlock))
+                                    );
+                                    hasCustomBlocks = true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (!requiresEdits.get())
+                    requiresEdits.set(hasCustomBlocks);
+            });
+
+            //TODO: why is sections iterated two times? WTF
+            if (requiresEdits.get())
+            {
+                // println("Requires edit, so we are going to clear the read buffer")
+                // Only rewrite the packet if we really need to edit the packet
+                ByteBuf byteBuf = Unpooled.buffer();
+                sections.forEach(chunkSection -> {
+                    if (chunkSection == null)
+                        return;
+
+                    try
+                    {
+                        chunkSectionType.write(byteBuf, chunkSection);
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                });
+
+                AbstractCustomBlocksManager.inst.writeByteArrayDataToLevelChunkDataPacket(
+                        chunkDataPacket,
+                        byteBuf.array()
+                );
+            }
+        }
+        else if (e.getPacketType() == PacketType.Play.Server.BLOCK_CHANGE)
+        {
+            ClientboundBlockUpdatePacket blockUpdate = (ClientboundBlockUpdatePacket) e.getPacket().getHandle();
+            if (AbstractCustomBlocksManager.inst.contains(blockUpdate.blockState.getBlock()))
+            {
+                CachedCustomBlockInfo cachedBlock = AbstractCustomBlocksManager.inst.get(blockUpdate.blockState.getBlock());
+                //blockState field
+                e.getPacket().getModifier().write(1, AbstractCustomBlocksManager.inst.nmsBlockFromCached(cachedBlock));
+            }
+        }
+        else if (e.getPacketType() == PacketType.Play.Server.MULTI_BLOCK_CHANGE)
+        {
+            BlockState[] blockStates = (BlockState[]) e.getPacket().getModifier().read(2);
+            // If there isn't any custom block in this packet, let's ignore it
+            if (containsNoCustomBlock(blockStates))
+                return;
+
+            for (int i = 0, changedBlocksLength = blockStates.length; i < changedBlocksLength; i++)
+            {
+                BlockState blockState = blockStates[i];
+                CachedCustomBlockInfo cachedBlock = (CachedCustomBlockInfo) AbstractCustomBlocksManager.inst.get(blockState.getBlock());
+                if (cachedBlock != null)
+                {
+                    // And add a wrapped block data!
+                    blockStates[i] = (BlockState) AbstractCustomBlocksManager.inst.nmsBlockFromCached(cachedBlock);
+                }
+                else
+                {
+                    blockStates[i] = blockState;
+                }
+            }
+        }
+    }
+
+    private boolean containsNoCustomBlock(DataPalette blockPalette)
+    {
+        for (int i = 0; i < blockPalette.size(); i++)
+        {
+            int paletteId = blockPalette.idByIndex(i);
+            if(AbstractCustomBlocksManager.inst.contains(paletteId))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean containsNoCustomBlock(BlockState[] changedBlocks)
+    {
+        for (BlockState changedBlock : changedBlocks)
+        {
+            if(AbstractCustomBlocksManager.inst.contains(changedBlock.getBlock()))
+                return false;
+        }
+        return true;
+    }
+}
