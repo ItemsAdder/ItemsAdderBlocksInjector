9c4d7134cc480c894e99e5f7be36c861aef5e9fc
LoneDev6 <leoxy98@gmail.com>
2022-12-19 13:38:55 +0100

    Fixes
    - cleaned up jar

--- itemsadder-blocks-injector-core/src/main/java/dev/lone/blocksinjector/annotations/NotNull.java
+++ itemsadder-blocks-injector-core/src/main/java/dev/lone/blocksinjector/annotations/NotNull.java
@@ -2,7 +2,6 @@ package dev.lone.blocksinjector.annotations;
 
 import java.lang.annotation.*;
 
-@Documented
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE})
 public @interface NotNull {}
